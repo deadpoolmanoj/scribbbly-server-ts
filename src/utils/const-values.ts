187a630ebdb63214d5db95d38cd27b810dc85312
Manojnaik123 <manojnaikbse@gmail.com>
2026-05-13 00:29:44 +0530

    added prod url

--- src/utils/const-values.ts
+++ src/utils/const-values.ts
@@ -2,15 +2,15 @@ import { LanguageType } from "./props/all-props";
 
 // export const ROUND_TIMER_VALUE : number = 30
 
-export const DEFAULT_ROUND_TIME : number = 1
+export const DEFAULT_ROUND_TIME : number = 10
 
-export const DEFAULT_ROUNDS : number = 1
+export const DEFAULT_ROUNDS : number = 2
 
-export const DEFAULT_WORD_SELECTION_TIME : number = 1
+export const DEFAULT_WORD_SELECTION_TIME : number = 5
 
 export const DEFAULT_LANGUAGE : LanguageType = 'ENGLISH'
 
 export const COUNTDOUN_UNIT: number = 1000
 
-export const POINTS_BOARD_TIME: number = 1
+export const POINTS_BOARD_TIME: number = 5
 
