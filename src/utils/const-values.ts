495bb6069f5f3673bbc4cbffba457e2cf2101cf7
Manojnaik123 <manojnaikbse@gmail.com>
2026-05-13 00:34:48 +0530

    changes const

--- src/utils/const-values.ts
+++ src/utils/const-values.ts
@@ -2,7 +2,7 @@ import { LanguageType } from "./props/all-props";
 
 // export const ROUND_TIMER_VALUE : number = 30
 
-export const DEFAULT_ROUND_TIME : number = 10
+export const DEFAULT_ROUND_TIME : number = 15
 
 export const DEFAULT_ROUNDS : number = 2
 
@@ -12,5 +12,5 @@ export const DEFAULT_LANGUAGE : LanguageType = 'ENGLISH'
 
 export const COUNTDOUN_UNIT: number = 1000
 
-export const POINTS_BOARD_TIME: number = 5
+export const POINTS_BOARD_TIME: number = 4
 
